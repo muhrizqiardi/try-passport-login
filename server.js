d6565841f9f130fb8fbec8fc2ddbd64c2f8200c9
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-05-24 22:31:56 +0700

    alot

--- server.js
+++ server.js
@@ -1,18 +1,54 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const initializePassport = require('./config/passport-config');
+
 const app = express();
+
+let users = []
 
 const PORT = process.env.PORT || 3001;
 
-app.set("view engine", 'ejs')
+//// View engine
+app.set("view engine", 'ejs');
 
+
+//// Middleware
+app.use(express.urlencoded({ extended: false }));
+
+
+//// Routes
+// Home
 app.get('/', (req, res) => {
-    res.render('index', {name: 'Bebek', })
+    res.render('index', {name: 'Bebek', });
 });
 
+// Login
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login');
 });
 
+// Register
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.post('/register', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch (error) {
+      console.log(error)
+      res.redirect('/register')
+    }
+    console.log(users)
+});
+
+//// Start server
 app.listen(PORT, () => {
   console.log("Listening to port " + PORT);
 })
